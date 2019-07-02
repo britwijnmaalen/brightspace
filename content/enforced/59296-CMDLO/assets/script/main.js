d635fb4c1fca97fb24662fcddacc3df72267d782
Justus Sturkenboom <justus@ju5tu5.nl>
2019-07-02 18:03:35 +0200

    trying to load the feeds

--- content/enforced/59296-CMDLO/assets/script/main.js
+++ content/enforced/59296-CMDLO/assets/script/main.js
@@ -4,20 +4,22 @@
 * Loosely based on:
 *   - https://dev.to/shoupn/javascript-fetch-api-and-using-asyncawait-47mp
 *   - https://dev.to/niinpatel/converting-xml-to-json-using-recursion-2k4j
+*   - 
 */
 
-
-// https://dlo.mijnhva.nl/d2l/le/news/rss/59296/course?ou=59296
-// http://www.hva.nl/faculteit/fdmci/nieuws/nieuwsoverzicht.rss
-
-
-async function getFeedAsync(url) {
-  return await fetch('https://cors-anywhere.herokuapp.com/'+url, {method: 'GET'})
+const feeds = {
+    cmdlo: "https://dlo.mijnhva.nl/d2l/le/news/rss/59296/course?ou=59296",
+    fdmci: "http://www.hva.nl/faculteit/fdmci/nieuws/nieuwsoverzicht.rss"
 }
 
-async function parseFeedAsync(data) {
-    console.log(data)
-}
+// async function getFeedAsync(url) {
+//   return await fetch('https://cors-anywhere.herokuapp.com/'+url, {method: 'GET'})
+// }
 
-getFeedAsync(`https://dlo.mijnhva.nl/d2l/le/news/rss/59296/course?ou=59296`)
-    .then(response => parseFeedAsync(response))
+// async function parseFeedAsync(data) {
+//     console.log(data)
+// }
+
+fetch(feeds.cmdlo)
+    .then(response => response.text())
+    .then(str => console.log(str))
