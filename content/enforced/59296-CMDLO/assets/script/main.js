0bc776bc588c18a3509e61adf9eb16b05b4251c0
joostf <j.faber@hva.nl>
2019-09-04 13:13:19 +0200

    clean up js & small style tweaks

--- content/enforced/59296-CMDLO/assets/script/main.js
+++ content/enforced/59296-CMDLO/assets/script/main.js
@@ -15,124 +15,132 @@
     local: './assets/script/rss.xml'
   };
 
-  // init UI functions
+  handleNews();
   enhanceUI();
 
   // fetch, parse en render rss news feed
-  // create another script that we only include in index?
-  if (document.body.contains($('#news'))) {
-    getNews();
-  }
-
-  // does way to much... but it works!
-  function getNews() {
-    const newsContainer = document.getElementById('news');
-    let url =
-      window.location.hostname === 'cmda.github.io' ||
-      window.location.hostname === 'localhost'
-        ? feeds.local
-        : feeds.cmdlo;
-
-    fetch(url)
-      .then(response => response.text())
-      .then(str => new window.DOMParser().parseFromString(str, 'text/xml'))
-      .then(xml => {
-        const data = xml2json(xml);
-        const items = data.rss.channel.item;
-        let news = '';
-
-        if (!items.length) {
-          news = template(items);
-        } else {
-          news = items
-            .map(item => {
-              return template(item);
-            })
-            .reverse()
-            .join('');
-        }
-
-        newsContainer.insertAdjacentHTML('afterend', news);
-      });
-  }
-
-  /**
-   * This function returns a rendered item
-   * @param item: item to render
-   */
-  function template(item) {
-    return `
-      <article>
-        <header>
-          <h1>${item.title}</h1>
-          <time>${convertDate(item.pubDate)}</time>
-
-        </header>
-
-        <p>${stripHTML(item.description)}</p>
-
-      </article>
-    `;
-  }
-  /**
-   * This function strips HTML elements from string
-   * @param str: string to strip
-   */
-  function stripHTML(str) {
-    const doc = new DOMParser().parseFromString(str, 'text/html');
-    return doc.body.textContent || '';
-  }
-
-  /**
-   * This function converts a date into Intl.DateTimeFormat
-   * @param date: date to convert
-   */
-  function convertDate(date) {
-    return new Intl.DateTimeFormat().format(new Date(date));
-  }
-
-  function enhanceUI() {
-    // jump to site navigation
-    const toNav = $('a.to-navigation');
-    if (document.body.contains(toNav)) {
-      const nav = $('body > nav');
-      toNav.addEventListener('click', e => {
-        e.preventDefault();
-        setTimeout(scrollToContent(nav), 100);
-      });
+  function handleNews() {
+    if (document.body.contains($('#news'))) {
+      getNews();
     }
 
-    // scroll past inline submenu on contentpages
-    const onPageNav = $('main header > nav');
-    if (document.body.contains(onPageNav)) {
-      const target = $('main header + *');
-      setTimeout(scrollToContent(target), 2000);
-    }
+    // does way to much... but it works!
+    function getNews() {
+      const newsContainer = document.getElementById('news');
+      let url =
+        window.location.hostname === 'cmda.github.io' ||
+        window.location.hostname === 'localhost'
+          ? feeds.local
+          : feeds.cmdlo;
 
-    // scroll to inline page navigation
-    const toInlineNav = $$('main article > a');
-    if (document.body.contains(toInlineNav[0])) {
-      const inlineNav = $('main > nav');
-      toInlineNav.forEach(link => {
-        link.addEventListener('click', e => {
-          e.preventDefault();
-          setTimeout(scrollToContent(inlineNav), 100);
+      fetch(url)
+        .then(response => response.text())
+        .then(str => new window.DOMParser().parseFromString(str, 'text/xml'))
+        .then(xml => {
+          const data = xml2json(xml);
+          const items = data.rss.channel.item;
+          let news = '';
+
+          if (!items.length) {
+            news = template(items);
+          } else {
+            news = items
+              .map(item => {
+                return template(item);
+              })
+              .reverse()
+              .join('');
+          }
+
+          newsContainer.insertAdjacentHTML('afterend', news);
         });
-      });
+    }
+
+    /**
+     * This function returns a rendered item
+     * @param item: item to render
+     */
+    function template(item) {
+      return `
+        <article>
+          <header>
+            <h1>${item.title}</h1>
+            <time>${convertDate(item.pubDate)}</time>
+
+          </header>
+
+          <p>${stripHTML(item.description)}</p>
+
+        </article>
+      `;
     }
     /**
-     * This function creates a button to toggle the navigation using a CSS
-     * class juggling event listener toggling the 'hide-menu' class.
-     *
-     * DOES TOO MUCH, REFACTOR!
+     * This function strips HTML elements from string
+     * @param str: string to strip
      */
-    function toggleNav() {
-      let toggleBtn = createElement('button');
-      setInnerHTML(toggleBtn, 'toggle menu');
-      toggleBtn.addEventListener('click', e =>
-        document.body.classList.toggle('hide-menu')
-      );
-      append(toggleBtn, 'nav');
+    function stripHTML(str) {
+      const doc = new DOMParser().parseFromString(str, 'text/html');
+      return doc.body.textContent || '';
+    }
+
+    /**
+     * This function converts a date into Intl.DateTimeFormat
+     * @param date: date to convert
+     */
+    function convertDate(date) {
+      return new Intl.DateTimeFormat().format(new Date(date));
+    }
+  }
+
+  // enhance UI
+  function enhanceUI() {
+    skipSubNav();
+    scrollToInlineNav();
+    scrollToNav();
+
+    /**
+     * This function makes it possible to scroll past the sub navigation
+     *
+     */
+    function skipSubNav() {
+      const onPageNav = $('main header > nav');
+      if (document.body.contains(onPageNav)) {
+        const target = $('main header + *');
+        setTimeout(scrollToContent(target), 2000);
+      }
+    }
+
+    /**
+     * This function makes it possible to scroll to the inline page navigation
+     *
+     */
+    function scrollToInlineNav() {
+      const toInlineNav = $$('main article > a');
+      if (document.body.contains(toInlineNav[0])) {
+        const inlineNav = $('main > nav');
+        toInlineNav.forEach(link => {
+          link.addEventListener('click', e => {
+            e.preventDefault();
+            setTimeout(scrollToContent(inlineNav), 100);
+          });
+        });
+      }
+    }
+
+    /**
+     * This function creates a link making it possible to scroll to the main navigation
+     *
+     */
+    function scrollToNav() {
+      let scrollToLink = createElement('a');
+      setInnerHTML(scrollToLink, '👇 navigatie');
+      scrollToLink.classList.add('to-navigation');
+      scrollToLink.addEventListener('click', e => {
+        e.preventDefault();
+        const nav = $('body > nav');
+        setTimeout(scrollToContent(nav), 100);
+      });
+      append(scrollToLink, 'main > header');
     }
 
     /**
@@ -170,7 +178,6 @@
       };
     }
   }
-  // skip to site navigation
 
   // helper functions
   function $(el) {
